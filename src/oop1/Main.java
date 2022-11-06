457fd41639e533f54348ff3bd906f893de9fe012
ocivelek <o.civelek5255@gmail.com>
2022-11-06 18:36:24 +0300

    private-setget

--- src/oop1/Main.java
+++ src/oop1/Main.java
@@ -3,34 +3,33 @@ package oop1;
 public class Main {
 
 	public static void main(String[] args) {
-		String mesaj = "Vade Oranı";
-
 		Product product1 = new Product();
-
-		product1.name = "Delonghi Kahve Makinesi";
-		product1.unitPrice = 7500;
-		product1.discount = 7;
-		product1.unitsInStock = 3;
-		product1.imageUrl = "bilmemne.jpg";
+		product1.setName("Delonghi Kahve Makinesi");
+		product1.setDiscount(7);
+		product1.setUnitPrice(7500);
+		product1.setUnitsInStock(3);
+		product1.setImageUrl("Image1.jpg");
 
 		Product product2 = new Product();
-		product2.name = "Smeg Kahve Makinesi";
-		product2.unitPrice = 6500;
-		product2.discount = 8;
-		product2.unitsInStock = 2;
-		product2.imageUrl = "bilmemne2.jpg";
+		product2.setName("Smeg Kahve Makinesi");
+		product2.setDiscount(7);
+		product2.setUnitPrice(7500);
+		product2.setUnitsInStock(3);
+		product2.setImageUrl("Image2.jpg");
 
 		Product product3 = new Product();
-		product3.name = "Kitchen Aid Kahve Makinesi";
-		product3.unitPrice = 4500;
-		product3.discount = 9;
-		product3.unitsInStock = 4;
-		product3.imageUrl = "bilmemne3.jpg";
+		product3.setName("Kitchen Aid Kahve Makinesi");
+		product3.setDiscount(7);
+		product3.setUnitPrice(7500);
+		product3.setUnitsInStock(3);
+		product3.setImageUrl("Image3.jpg");
 
+		System.out.println("<ul>");
 		Product[] products = { product1, product2, product3 };
 		for (Product product : products) {
-			System.out.println(product.name);
+			System.out.println("<li>" + product.getName() + "</li>");
 		}
+		System.out.println("</ul>");
 
 	}
 
